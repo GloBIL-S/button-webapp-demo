cf43fb72f90204474f907e1d62f70483f01bfbdc
Gloria Sgnaolin <gloriasgnaolin01@outlook.com>
2026-08-26 11:06:46 +0200

    Prima versione web app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Button Web App Demo
+
+Mini PWA statica per GitHub Pages e iPhone.
---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='button-demo-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
